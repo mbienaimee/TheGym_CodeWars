31819be39d9b2d9b98ae014062ac1c694613e4b2
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2024-11-13 13:58:46 +0200

    project

--- Kata8/sum.js
+++ Kata8/sum.js
@@ -1,12 +1,9 @@
 function sum (numbers) {
   
     let summation = numbers.reduce((a,b)=>{
-      if(numbers === []){
-        return 0
-      }
-      else{
+     
          return a+b
-      }
+      
      
     },0)
     return summation;
