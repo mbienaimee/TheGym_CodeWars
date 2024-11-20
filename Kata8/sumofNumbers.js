f2631c8d1c7ada7fdd35d05d975c688ef5a29dc6
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2024-11-20 13:09:17 +0200

    kata 8

---
+++ Kata8/sumofNumbers.js
@@ -0,0 +1,14 @@
+function getSum(a, b)
+{
+  let result = 0
+   //Good luck!
+  if(a!==b){
+    result = a+b
+    
+  }
+  else if(a===b){
+    result =a
+  }
+  return result;
+}
+console.log(getSum(2,3))
