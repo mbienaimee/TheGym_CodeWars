8b305240d0c1e8752855c9bb49ef170c2907b8c2
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-02-22 13:32:15 +0200

    data and algorithm

--- Datastructure/fact.js
+++ Datastructure/fact.js
@@ -14,3 +14,16 @@ const fibb = (n) => {
   return fib;
 };
 console.log(fibb(4));
+// prime number
+const isPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isPrime(5));
