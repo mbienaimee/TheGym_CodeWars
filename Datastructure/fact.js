669ad261744f875caebb78ff95d722b07fdd2754
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-02-22 12:36:40 +0200

    project

---
+++ Datastructure/fact.js
@@ -0,0 +1,16 @@
+const fact = (n) => {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+};
+console.log(fact(5));
+const fibb = (n) => {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib;
+};
+console.log(fibb(4));
