37e508f924c0bafe6b4e775c74ada068dc776c32
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-02-22 15:16:46 +0200

    algoritm

--- Datastructure/fact.js
+++ Datastructure/fact.js
@@ -27,3 +27,26 @@ const isPrime = (n) => {
   return true;
 };
 console.log(isPrime(5));
+
+// is power of two
+const isPrime = (n) => {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+};
+console.log(isPrime(2));
+//recursive function
+const fibrecursive = (n) => {
+  if (n < 2) {
+    return n;
+  }
+  return fibrecursive(n - 2) + fibrecursive(n - 1);
+};
+console.log(fibrecursive(4));
